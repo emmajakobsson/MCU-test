d2e49ffb65172a0181ee9d482b4ee0df1ec37081
Emma <lee-emma@live.se>
2021-10-14 15:31:18 +0200

    cleaning of code

--- main/main.c
+++ main/main.c
@@ -12,21 +12,14 @@ void app_main(void)
         vTaskDelay(100);
     }*/
     uint8_t reg_addr = 0xFF; //output register 0x24
-    uint8_t data[82] = {-2};
-    int iter = 0;
+    uint8_t data[50] = {-2}; //82 highest value
     int flag = 0;
-    //esp_err_t err;
 
-    //configure i2c master
     configure_i2c_master();
-    //configure IMU/sensor
-    //configure_imu();
     
     while(1){
         i2c_master_write_read_device(I2C_MASTER_NUM, SLAVE_ADDR, &reg_addr, 1, data, sizeof(data), I2C_MASTER_TIMEOUT_MS/portTICK_RATE_MS);
-        //err = read_master_imu(&data);
-        //printf("\e[1;1H\e[2J"); //clear terminal, keep print to one line
-        //printf("iteration: %d\n", iter);
+
         for(int i = 0; i < 10; i++){
             if(data[i] == 71 && data[i + 1] == 76 && data[i + 2] == 76){
                 flag = 1;
@@ -34,23 +27,16 @@ void app_main(void)
             }
         }
 
-        while(data[iter] != 10){
-            iter++;
+        for(int j = 0; j < 50; j++){
+            if(flag == 1)
+                printf("%c", (char)data[j]); //02x hhn
         }
 
-        if(iter == 50){
-            for(int j = 0; j < iter; j++){
-                if(flag == 1)
-                    printf("%c", (char)data[j]); //02x hhn
-            }
-        }
-
-        if(flag == 1 && iter == 50){
+        if(flag == 1){
             printf("\n");
         }
 
         flag = 0;
-        iter = 0;
         memset(data,-2,sizeof(uint8_t));
         //vTaskDelay(100);
     }
