43fda50a4333288ee4ab869a045d7c4339005a24
Erik <eln17011@student.mdh.se>
2021-10-15 09:28:58 +0200

    Commenting and changing memset
    Changed the default memset value to see if it fixes some junk values. Added comments in code

--- main/main.c
+++ main/main.c
@@ -11,6 +11,7 @@ void app_main(void)
         printf("Sensor reading: %d\n",data);
         vTaskDelay(100);
     }*/
+
     uint8_t reg_addr = 0xFF; //output register 0x24
     uint8_t data[82] = {-2}; //82 highest value
     int flag = 0;
@@ -18,12 +19,16 @@ void app_main(void)
     configure_i2c_master();
     
     while(1){
+
+        //read data from the sensor
         i2c_master_write_read_device(I2C_MASTER_NUM, SLAVE_ADDR, &reg_addr, 1, data, sizeof(data), I2C_MASTER_TIMEOUT_MS/portTICK_RATE_MS);
 
+        //print the data from the buffer
         for(int j = 0; j < 82; j++){
             printf("%c", (char)data[j]); //02x hhn
         }
 
+        //filtering of messages
         /*for(int i = 0; i < 10; i++){
             if(data[i] == 71 && data[i + 1] == 76 && data[i + 2] == 76){
                 flag = 1;
@@ -39,7 +44,8 @@ void app_main(void)
         }
 
         flag = 0;*/
-        memset(data,-2,sizeof(uint8_t));
+        //reset the buffer with values
+        memset(data,94,sizeof(uint8_t));
         //vTaskDelay(100);
     }
 }
