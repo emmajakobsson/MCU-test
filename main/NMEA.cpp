084f4884ac406586a0246ad477222e0866f1b50e
Erik <eln17011@student.mdh.se>
2021-10-21 13:34:09 +0200

    Added the NMEA library file
    It can check for valid messages

---
+++ main/NMEA.cpp
@@ -0,0 +1,18 @@
+#include "NMEA.h"
+
+string NMEA_messages[3] = {"GGA","GLL","RMC"};
+
+int validMessage(string data){
+    
+    int size = 0;
+    size = sizeof(NMEA_messages)/sizeof(NMEA_messages[0]);
+
+    for(int i = 0; i < size; i++){
+        if (data.find(NMEA_messages[i]) != string::npos)
+            return 1;
+        }
+    
+    return 0;
+
+}
+
---
+++ main/NMEA.h
@@ -0,0 +1,12 @@
+#include <iostream>     // std::cout
+#include <algorithm>    // std::find
+#include <string.h>
+
+using namespace std;
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int validMessage(string data);
+
+#endif
