827f49eda5bf24dc843cfd38a5a8e2965bcaf1ed
Emma <lee-emma@live.se>
2021-10-21 14:25:38 +0200

    added uart and changed to cpp

---
+++ main/main.cpp
@@ -0,0 +1,42 @@
+extern "C"{
+    #include "func.h"
+}
+
+void app_main(void)
+{
+    //int data = -2;
+
+    //configure_sensor();
+
+    /*while(1){
+        data = adc1_get_raw(ADC1_CHANNEL_5);
+        printf("Sensor reading: %d\n",data);
+        vTaskDelay(100);
+    }*/
+
+    uint8_t reg_addr = 0xFF; //output register 0x24
+    uint8_t data[82] = {94}; //82 highest value
+
+    configure_i2c_master();
+
+    while(1){
+        //I2C
+        //read data from the sensor
+        i2c_master_write_read_device(I2C_MASTER_NUM, SLAVE_ADDR, &reg_addr, 1, data, sizeof(data), I2C_MASTER_TIMEOUT_MS/portTICK_RATE_MS);
+
+        //print the data from the buffer
+        for(int j = 0; j < 82; j++){
+            printf("%c", (char)data[j]); //02x hhn
+        }
+
+        //UART
+        /*uart_read_bytes(UART_NUM_0, data, sizeof(data), 20 / portTICK_RATE_MS);
+        for(int j = 0; j < 82; j++){
+            printf("%c", (char)data[j]); //02x hhn
+        }*/
+
+        //reset the buffer with values
+        memset(data,94,sizeof(uint8_t));
+        vTaskDelay(50);
+    }
+}
