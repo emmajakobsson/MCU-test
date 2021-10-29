7aa54a72ab4d3704a3f82bd245fddb857fca809c
Erik <eln17011@student.mdh.se>
2021-10-29 15:27:00 +0200

    added spaces

--- components/nmea/nmea.c
+++ components/nmea/nmea.c
@@ -2,25 +2,24 @@
 
 int match(char * buf, char * pattern, regmatch_t * pmatch){
 
+    regex_t    preg;
 
-regex_t    preg;
-
-int        rc;
-size_t     nmatch = 2;
+    int        rc;
+    size_t     nmatch = 2;
 
 
-if (0 != (rc = regcomp(&preg, pattern, REG_EXTENDED))) {
-    printf("regcomp() failed, returning nonzero (%d)\n", rc);
-    exit(EXIT_FAILURE);
-}
+    if (0 != (rc = regcomp(&preg, pattern, REG_EXTENDED))) {
+        printf("regcomp() failed, returning nonzero (%d)\n", rc);
+        exit(EXIT_FAILURE);
+    }
 
-if (0 != (rc = regexec(&preg, buf, nmatch, pmatch, 0))) {
-    printf("Failed to match '%s' with '%s',returning %d.\n",
-            buf, pattern, rc);
-}
+    if (0 != (rc = regexec(&preg, buf, nmatch, pmatch, 0))) {
+        printf("Failed to match '%s' with '%s',returning %d.\n",
+                buf, pattern, rc);
+    }
 
-regfree(&preg);
-return 1;
+    regfree(&preg);
+    return 1;
 
 }
 
@@ -30,27 +29,12 @@ int parse(char * buf, char * pattern){
     if(!match(string, pattern, pmatch)){
         return 0;
     }
+    float  = 
+    int new_start = pmatch[0].rm_eo + 1;
 
-
+    if(!match(string, pattern, pmatch)){
+        return 0;
+    }
 }
 
-
-
-/*int match(char * buf, char * rexp){
-
-    regex_t re;
-
-    if(regcomp(&re, rexp, REG_EXTENDED) != 0){
-        regfree(&re);
-        return 0;
-    }
-        
-    if(regexec(&re, buf, (size_t) 0, NULL, 0) != 0){
-        regfree(&re);
-        return 0;
-    }
-    regfree(&re);
-
-    return 1; 
-}*/
 
